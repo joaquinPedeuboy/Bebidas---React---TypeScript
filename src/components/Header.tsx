c716c7e067e8d9b4af51a0731baa6a64f4238b25
Joaquin Pedeuboy <joaquin.pedeuboy@hotmail.com>
2025-10-06 19:04:01 -0300

    Añadiendo Outlet

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Header() {
+    return (
+        <header className="bg-slate-800">
+            <div className="mx-auto container px-5 py-16">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <img src="/logo.svg" alt="logotipo" className="w-32" />
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -3,6 +3,8 @@
 
 export default function FavoritesPage() {
   return (
-    <div>FavoritesPage</div>
+    <>
+      
+    </>
   )
 }
